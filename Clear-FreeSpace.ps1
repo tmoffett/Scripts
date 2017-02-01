da0a1eeed3e86185314d9ff43d4cd8ca1e4ed959
Ty Moffett <ty.moffett@gmail.com>
2017-02-01 15:33:20 -0600

    Added Configure-CleanMgr and thought I had Run-CleanMgr working...

--- Clear-FreeSpace.ps1
+++ Clear-FreeSpace.ps1
@@ -30,6 +30,8 @@ function Clear-DiskSpace
         $FreeSpaceBefore = $null
         $FreeSpaceAfter = $null
         $PSRemoting = $null
+        $Session = $null
+        $StateFlag = "StateFlag0023"
     }
     
     Process
@@ -39,14 +41,24 @@ function Clear-DiskSpace
             #Does PSRemoting work?       
             [bool]$PSRemoting = Test-PSRemoting -Computername $ComputerName
             Write-Verbose "PSRemoting test result is $PSRemoting"
+
+            $Session = New-PSSession -ComputerName $ComputerName
             
             #Get the beginning free space.
             $FreeSpaceBefore = Get-FreeSpace -ComputerName $ComputerName
-            
-            
+
+            Configure-CleanMgr -Session $Session -StateFlag $StateFlag
+
+            Run-CleanMgr -ComputerName $ComputerName -Session $Session
+
+            Remove-PSSession -Session $Session
+
+            $FreeSpaceAfter = Get-FreeSpace -ComputerName $ComputerName
+                        
             #Collect all of the various properties we want to hang on to.
             $properties = @{ComputerName = $ComputerName
-                            FreeSpaceBefore = $FreeSpaceBefore}
+                            FreeSpaceBefore = $FreeSpaceBefore
+                            FreeSpaceAfter = $FreeSpaceAfter}
 
             #Create a new object and add the $properties to it.
             $obj = New-Object -TypeName PSObject -Property $properties
@@ -161,11 +173,11 @@ function Run-CleanMgr
 {
 <#
 .Synopsis
-   This shit don't work yo.
+   Run Cleanmgr.exe (Disk Cleanup) using default selections.
 .DESCRIPTION
    Long description
 .EXAMPLE
-   Example of how to use this cmdlet
+   Run-CleanMgr -Computer COMPUTERNAME -Session SESSIONNAME
 .EXAMPLE
    Another example of how to use this cmdlet
 #>
@@ -179,7 +191,7 @@ function Run-CleanMgr
                    ValueFromPipelineByPropertyName=$true,
                    Position=0)]
         $ComputerName,
-        [System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty
+        $Session
     )
 
     Begin
@@ -189,10 +201,10 @@ function Run-CleanMgr
     Process
     {
         Try {
-            $CleanMgr = Invoke-Command -ComputerName $ComputerName -ErrorAction Stop -ErrorVariable EV -ScriptBlock {
-                    Write-Verbose "Does this thing work?"
-                    Start-Process -FilePath Cleanmgr -ArgumentList '/sagerun:1' -Wait -ErrorAction Stop -ErrorVariable EV
-                    #Write-Output $true
+            $CleanMgr = Invoke-Command -Session $Session -ErrorAction Stop -ScriptBlock {
+                    Write-Verbose "Starting cleanmgr.exe." -Verbose
+                    Start-Process -FilePath Cleanmgr.exe -ArgumentList '/sagerun:0023' -Wait -ErrorAction Stop -ErrorVariable EV | Wait-Process
+                    Write-Output $true
                 }
              }
 
@@ -205,5 +217,82 @@ function Run-CleanMgr
     {
         Write-Verbose "The value of the CleanMgr variable is: $CleanMgr"
         Write-Verbose $Error[0].Exception.GetType().FullName
+    }
+}
+
+
+function Configure-CleanMgr
+{
+<#
+.Synopsis
+   Configure CleanMgr.exe/Disk Cleanup
+.DESCRIPTION
+   Create StateFlag registry entries to enable the various portions of CleanMgr.exe.
+.EXAMPLE
+   Configure-CleanMgr -ComputerName COMPUTERNAME
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Session,
+
+        # Param2 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        $StateFlag
+    )
+
+    Begin
+    {
+        
+    }
+    Process
+    {
+        Write-Verbose "Starting Configure-CleanMgr"
+        $Config = Invoke-Command -Session $Session -ArgumentList $StateFlag -ErrorAction Stop -ScriptBlock {
+            param($StateFlag)
+            
+            Try
+            {
+                Write-Verbose "Clearing CleanMgr.exe automation settings." -Verbose
+                Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\*' -Name $StateFlag -ErrorAction SilentlyContinue | Remove-ItemProperty -Name $StateFlag -ErrorAction SilentlyContinue
+            }
+
+            Catch
+            {
+                Write-Warning "[CATCH] Errors found during attempt:`n$_"
+            }        
+            
+            
+            Try
+            {
+
+                Write-Verbose "Set a StateFlag for each available volume cache in order to enable each option in Cleanmgr." -Verbose
+                $VolumeCache = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\*'
+                foreach ($v in $VolumeCache) 
+                    { 
+                        New-ItemProperty -Path $v.PSPath -Name $StateFlag -Value 2 -PropertyType DWord
+                    }
+
+                Write-Verbose "Ending Configure-CleanMgr"
+            }
+
+            Catch
+            {
+                Write-Warning "[CATCH] Errors found during attempt:`n$_"
+            }
+        }
+    }
+    End
+    {
+        Write-Verbose "Ending Configure-CleanMgr."
     }
 }
